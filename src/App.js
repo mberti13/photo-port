5c09bd63cfd52f8994b414b94ef786d6478c112b
MJB <matt.berti13@gmail.com>
2022-07-01 21:46:24 -0500

    created about and nav section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+
+// import components
 import About from './components/About';
+import Nav from './components/Nav';
+
 
 import './App.css';
 
@@ -7,6 +11,7 @@ function App() {
 
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
