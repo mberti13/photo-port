85c29a0cfb74c2003260b7b0190a82892cc51344
MJB <matt.berti13@gmail.com>
2022-07-02 20:39:25 -0500

    finished module but photos not displaying

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,26 +1,15 @@
-import React from "react";
+import React from 'react';
+import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from '../../assets/small/commercial/0.jpg';
 
-function Gallery(){
-
-    const currentCategory ={
-        name: 'Commercial',
-        description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    };
-    return(
-        <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.name}</p>
-            <div className="flex-row">
-                <img src={photo}
-                alt='example'
-                className="img-thumbnail mx-1"
-                ></img>
-            </div>
-        </section>
-    );
+function Gallery(props) {
+  const { currentCategory } = props;
+  return (
+    <section>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
+    </section>
+  );
 }
-
 export default Gallery;
