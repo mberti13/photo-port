d0fdb3cfc73f9fc479f93813114db6a436c60c3c
MJB <matt.berti13@gmail.com>
2022-07-09 13:06:44 -0500

    completed photo gallery

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -6,27 +6,47 @@ import '@testing-library/jest-dom/extend-expect';
 
 import Nav from '..';
 
+// test for nav categories array
+const categories = [
+    {name: 'portraits', description: 'Portraits of people in my life'}
+]
+
+const mockCurrentCategory =jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () =>{
 
     // * baseline test
     it('renders', () =>{
-        render(<Nav></Nav>)
+        render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            />)
     });
 
-    // * snapshot test
-    it('renders Nav component to DOM node structure', () =>{
-        const { asFragment } = render(<Nav></Nav>);
-        //assert value comparison
-        expect(asFragment()).toMatchSnapshot();
-    });
-});
+//     // * snapshot test
+//     it('renders Nav component to DOM node structure', () =>{
+//         const { asFragment } = render(<Nav
+//             categories={categories}
+//             setCurrentCategory={mockSetCurrentCategory}
+//             currentCategory={mockCurrentCategory}
+//             />)
+//         //assert value comparison
+//         expect(asFragment()).toMatchSnapshot();
+//     });
+// });
 
 // * test for emoji visibility
 // describe('emoji is visible', () => {
 //     it('inserts emoji into the h2', () => {
-//     const { getByLabelText } = render(<Nav />);
+//     const { getByLabelText } = render(render(<Nav
+//         categories={categories}
+//         setCurrentCategory={mockSetCurrentCategory}
+//         currentCategory={mockCurrentCategory}
+//         />));
   
 //     expect(getByLabelText('camera')).toHaveTextContent('📸');
 //     });
@@ -35,8 +55,21 @@ describe('Nav component', () =>{
 // * test for link visibility/functionality
 // describe('links are visible', () =>{
 //     it('inserts text into links', () =>{
-//         const {getByTestId}  = render(<Nav></Nav>);
+//         const {getByTestId}  = render(render(<Nav
+//             categories={categories}
+//             setCurrentCategory={mockSetCurrentCategory}
+//             currentCategory={mockCurrentCategory}
+//             />));
 //         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
 //         expect(getByTestId('about')).toHaveTextContent('About me');
 //     });
 // });
+
+// test for nav categories array
+it('renders', () => {
+    render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+    />);
+  })
