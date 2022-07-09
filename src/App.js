5b1fb51645258900f9a9b11b4305de36d7a3bc6b
MJB <matt.berti13@gmail.com>
2022-07-09 12:54:05 -0500

    photos now display in gallery

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]); 
+  ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
